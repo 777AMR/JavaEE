732e4eea9373c1cae00e33f54f97e730d9fee98c
777AMR <meyram.ramazanov@mail.ru>
2021-08-17 20:19:42 +0600

    Java EE для начинающих. Урок 10: JDBC (часть 1). edit

---
+++ src/main/java/FirstServlet.java
@@ -0,0 +1,33 @@
+import somePackage.Cart;
+
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "FirstServlet")
+public class FirstServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        HttpSession session = request.getSession();
+
+        Cart cart = (Cart) session.getAttribute("cart");
+        String name = request.getParameter("name");
+        int quantity = Integer.parseInt(request.getParameter("quantity"));
+
+        if (cart == null) {
+            cart = new Cart();
+
+            cart.setName(name);
+            cart.setQuantity(quantity);
+        }
+        session.setAttribute("cart", cart);
+
+        getServletContext().getRequestDispatcher("/showCart.jsp").forward(request, response);
+
+//        PrintWriter pw = response.getWriter();
+//        pw.println("<html>");
+//        pw.println("</html>");
+    }
+}
